ebbb72416d9e1cbab43bd6dad7d527c1f16efb36
Jörg Jooss <joerg@joergjooss.de>
2024-04-22 09:20:24 +0200

    Remove --no-proxy again, not supported in Busybox

--- deploy/migrate.sh
+++ deploy/migrate.sh
@@ -5,7 +5,7 @@ wait_times="0 2 6 14 30"
 
 for t in $wait_times
 do
-  export PGPASSWORD=$(wget -q -O- --no-proxy --header "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://ossrdbms-aad.database.windows.net&client_id=$CLIENT_ID" \
+  export PGPASSWORD=$(wget -q -O- --header "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://ossrdbms-aad.database.windows.net&client_id=$CLIENT_ID" \
   | awk -F'"' '/access_token/ {print $4}')
   if [ $? = 0 ]; then break; fi;
   echo "Error fetching token, retrying after $t seconds"
