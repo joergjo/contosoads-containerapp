0ff92e8fde83ef89a6fd9c14baada200feace0f4
Jörg Jooss <joerg@joergjooss.de>
2026-01-02 11:23:43 +0100

    Update Testcontainers.*

--- test/ContosoAds.Web.Tests/PostgreSqlContainerFixture.cs
+++ test/ContosoAds.Web.Tests/PostgreSqlContainerFixture.cs
@@ -6,12 +6,10 @@ using Xunit.Sdk;
 
 namespace ContosoAds.Web.Tests;
 
-public class PostgreSqlContainerFixture(IMessageSink sink) : DbContainerFixture<PostgreSqlBuilder, PostgreSqlContainer>(sink)
+public class PostgreSqlContainerFixture(IMessageSink sink)
+    : DbContainerFixture<PostgreSqlBuilder, PostgreSqlContainer>(sink)
 {
     public override DbProviderFactory DbProviderFactory => NpgsqlFactory.Instance;
 
-    protected override PostgreSqlBuilder Configure(PostgreSqlBuilder builder)
-    {
-        return builder.WithImage("postgres:17-alpine");
-    }
+    protected override PostgreSqlBuilder Configure() => new("postgres:17-alpine");
 }
